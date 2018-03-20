02305b236510a1efeb46ee6ad0bef797ad234dd3
Osman Medzhytov <medjitov10@gmail.com>
2018-03-20 12:54:13 -0400

    q

--- front_end/index.js
+++ front_end/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-import { Router, browserHistory } from 'react-router';
+// import { Router, browserHistory } from 'react-router';
 
 import reducers from './reducers/reducers';
-import routes from './routes';
+// import routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-
+class Test extends Component {
+  render() {
+    return (
+      <div>Test</div>
+    )
+  }
+}
 
 
 
@@ -19,6 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('content');
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <Router history={browserHistory} routes={routes}/>
+      {/* <Router history={browserHistory} routes={routes}/> */}
+      <Test />
     </Provider>, root);
 });
