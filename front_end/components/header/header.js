58e32d82e527cefa5b32c46d19b1aec87f072f7e
Osman Medzhytov <medjitov10@gmail.com>
2018-03-27 12:16:22 -0400

    q

--- front_end/components/header/header.js
+++ front_end/components/header/header.js
@@ -17,9 +17,15 @@ class Header extends Component {
     return (
       <div className='header'>
         <div className='header-content'>
-          <Link className='header-content-a' to="/"><div className='logo-img'></div></Link>
-          <UserSearch />
-          <a href={`/${nickname}`}><div className='profile-icon'></div></a>
+          <div className='header-item-qwe'>
+            <Link className='header-content-a' to="/"><div className='logo-img'></div></Link>
+          </div>
+          <div className='header-item-qwe'>
+            <UserSearch />
+          </div>
+          <div className='header-item-qwe'>
+            <a href={`/${nickname}`}><div className='profile-icon'></div></a>
+          </div>
         </div>
       </div>
     );
